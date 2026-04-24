01500b82bb2ec6cc187dc911b034e7fc92336325
Leo Huber <leo_huber@icloud.com>
2026-04-24 17:20:39 +0200

    feat: add configuration retrieval to CLI main function

--- src/doc_classify/cli.py
+++ src/doc_classify/cli.py
@@ -4,11 +4,14 @@ from importlib.metadata import version
 
 import click
 
+from doc_classify.config import get_config
+
 
 @click.group(invoke_without_command=True)
 @click.version_option(version=version("doc-classify"), prog_name="doc-classify")
 @click.pass_context
 def main(ctx: click.Context) -> None:
     """Doc-classify command-line interface."""
+    config = get_config()  # noqa: F841
     if not ctx.invoked_subcommand:
         click.echo(ctx.get_help())
